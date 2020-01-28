fa3af9103f398bc68156bb0047f85a3613fada30
kborojerdi <38837474+kborojerdi@users.noreply.github.com>
2020-01-28 04:38:14 -0800

    Update and rename mqtt.py to mqtt-forwarder.py

--- HW3/jetson/mqtt-forwarder.py
+++ HW3/jetson/mqtt-forwarder.py
@@ -2,11 +2,11 @@ import paho.mqtt.client as mqtt
 
 LOCAL_MQTT_HOST="mosquitto"
 LOCAL_MQTT_PORT=1883
-LOCAL_MQTT_TOPIC="test"
+LOCAL_MQTT_TOPIC="system/jetson/webcam/face"
 
 REMOTE_MQTT_HOST="169.62.93.58"
 REMOTE_MQTT_PORT=1883
-REMOTE_MQTT_TOPIC="test"
+REMOTE_MQTT_TOPIC="system/jetson/webcam/face"
 
 def on_connect_local(client, userdata, flags, rc):
         print("connected to local broker with rc: " + str(rc))
@@ -19,7 +19,7 @@ def on_connect_remote(client, userdata, flags, rc):
 def on_message(client,userdata, msg):
   try:
     print("message received!")	
-    # if we wanted to re-publish this message, something like this should work
+    # re-publish the message
     msg = msg.payload
     remote_mqttclient.publish(REMOTE_MQTT_TOPIC, payload=msg, qos=0, retain=False)
   except:
@@ -27,13 +27,14 @@ def on_message(client,userdata, msg):
 
 local_mqttclient = mqtt.Client("LocalClient-01")
 local_mqttclient.on_connect = on_connect_local
-local_mqttclient.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_PORT, 300)
+local_mqttclient.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_PORT, 60)
 
 remote_mqttclient = mqtt.Client("RemoteClient-01")
 remote_mqttclient.on_connect = on_connect_remote
-remote_mqttclient.connect(REMOTE_MQTT_HOST, REMOTE_MQTT_PORT, 300)
+remote_mqttclient.connect(REMOTE_MQTT_HOST, REMOTE_MQTT_PORT, 60)
 
 local_mqttclient.on_message = on_message
 
-# go into a loop
+# go into a loop to maintain connection
 local_mqttclient.loop_forever()
+remote_mqttclient.loop_forever()
